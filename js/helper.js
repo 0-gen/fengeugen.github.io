4a80c6fe39371d02682940e8c207a59f7c9a3330
Eugen Feng <fengeugen@gmail.com>
2026-09-04 17:02:29 -0400

    updated about me page

--- js/helper.js
+++ js/helper.js
@@ -21,3 +21,33 @@ function slideToAbout() {
       }, { once: true });
     }, { once: true }); // Ensure the event only fires once
 }
+
+const sidebar = document.querySelector(".sidebar");
+const showContactsButton = document.querySelector(".info_more-btn");
+
+showContactsButton.addEventListener("click", () => {
+    sidebar.classList.toggle("active");
+});
+
+const track = document.querySelector(".carousel-track");
+
+let position = 0;
+
+function slidePhotos() {
+    position -= 1;
+
+    track.style.transform = `translateX(${position}px)`;
+
+    // Reset once the photos have moved far enough
+    if (Math.abs(position) >= track.scrollWidth / 2) {
+        position = 0;
+        track.style.transition = "none";
+        track.style.transform = "translateX(0)";
+
+        requestAnimationFrame(() => {
+            track.style.transition = "transform 0.1s linear";
+        });
+    }
+}
+
+setInterval(slidePhotos, 30);
